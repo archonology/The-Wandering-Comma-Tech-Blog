6fdd6192184120b95aa287caee5626cdbc77662b
Your Name <you@example.com>
2022-10-09 15:35:49 -0500

    issue corrected with user id (random not needed)

--- seeds/seed.js
+++ seeds/seed.js
@@ -15,7 +15,7 @@ const seedDatabase = async () => {
   for (const blog of blogData) {
     await Blog.create({
       ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      // user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
 
