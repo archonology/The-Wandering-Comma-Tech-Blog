5cf3967cadb2ec18fea666d243ad4caace0a3ffe
Your Name <you@example.com>
2022-10-10 11:37:46 -0500

    comments, keys, and seeds

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
+//blogs belong to users
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+//a user hasMany blogs
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Blog.belongsTo(User, {
-  foreignKey: 'user_id'
+//comments belong to blogs
+Comment.belongsTo(Blog, {
+  foreignKey: "blog_id",
 });
 
-module.exports = { User, Blog };
+//a blog hasMany comments
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Blog, Comment };
