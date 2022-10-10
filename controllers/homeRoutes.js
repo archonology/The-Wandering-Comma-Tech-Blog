8d2d770aac94f7ef9bd782be8f25ee56cebff844
Your Name <you@example.com>
2022-10-09 23:06:03 -0500

    working on single posts route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
     try {
         const dbBlogData = await Blog.findAll({
-            attributes: ['title', 'post', 'date_created'],
+            attributes: ['id', 'title', 'post', 'date_created'],
         });
 
         const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
@@ -23,6 +23,20 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const dbBlogData = await Blog.findByPk(req.params.id, {
+            attributes: ['id', 'title', 'post', 'date_created'],
+        });
+
+        const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('posts', { blogs, logged_in: req.session.logged_in,});
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
