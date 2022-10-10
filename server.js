604f96ee84a01dda6bc9d982b888e25dee50974d
Your Name <you@example.com>
2022-10-09 21:04:17 -0500

    working on site activation

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -20,8 +22,14 @@ const sess = {
 
 app.use(session(sess));
 
+//to use handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+ //for the static route path
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
