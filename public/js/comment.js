a8a0efda0bd1e7367807321cf1b8649aa588c1da
Your Name <you@example.com>
2022-10-12 00:07:35 -0500

    working on comment script

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment = document.querySelector('#comment').value.trim();
+    const username = document.querySelector('#username').value.trim();
+  
+    if (comment && username) {
+      // do I have the path I need in routes?
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ comment, username }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to post comment.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
+  
